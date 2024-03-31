64c3818bec27dfc2407a700b1630bfd32f413c9f
1201391 <1201391@isep.ipp.pt>
2024-03-31 12:48:49 +0100

    Updated app component and app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CreateRequestComponent} from "./create-request/create-request.component";
 import {ListPendingRequestComponent} from "./list-pending-request/list-pending-request.component";
+import {ProcessRequestComponent} from "./process-request/process-request.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: Routes = [
   { path: 'createRequest', component: CreateRequestComponent  },
   { path: 'pendingRequests', component: ListPendingRequestComponent  },
+  { path: 'Requests/:id', component: ProcessRequestComponent  },
+  { path: 'dashboard', component: DashboardComponent  },
+
 
   { path: '**', redirectTo: '' },
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import {CreateRequestComponent} from "./create-request/create-request.component"
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import {ListPendingRequestComponent} from "./list-pending-request/list-pending-request.component";
 import {ProcessRequestComponent} from "./process-request/process-request.component";
+import {Dashboard} from "./models/dashboard.model";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {CanvasJS, CanvasJSAngularChartsModule} from "@canvasjs/angular-charts";
 
 @NgModule({
   declarations: [
     CreateRequestComponent,
     ListPendingRequestComponent,
     ProcessRequestComponent,
+    DashboardComponent,
 
     AppComponent
   ],
@@ -23,6 +27,7 @@ import {ProcessRequestComponent} from "./process-request/process-request.compone
     BrowserModule,
     HttpClientModule,
     FormsModule, CommonModule, ReactiveFormsModule,HttpClientModule,
+    CanvasJSAngularChartsModule,
     NgMultiSelectDropDownModule.forRoot(),
 
     AppRoutingModule
