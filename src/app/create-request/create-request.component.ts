462808dc478d6cefb885de6e1fcf78093a6d7104
1201391 <1201391@isep.ipp.pt>
2024-03-31 12:48:09 +0100

    Updated create request component

--- src/app/create-request/create-request.component.ts
+++ src/app/create-request/create-request.component.ts
@@ -10,6 +10,7 @@ import {ProductService} from "../services/product/product.service";
 import {Product} from "../models/product.model";
 import {RequestItem} from "../models/request-item.model";
 import {RequestService} from "../services/request/request.service";
+import {Request} from "../models/request.model";
 
 @Component({
   selector: 'app-create-request',
@@ -21,10 +22,15 @@ export class CreateRequestComponent implements OnInit{
   requestForm!: FormGroup;
   products :  Product[] = [];
   items: RequestItem[] = [];
+  submittedRequest?: Request;
+
+  requestsToBeReceived: Request[] = [];
+
+  submitted: boolean = false;
 
   constructor(private fb: FormBuilder, private productService: ProductService, private requestService: RequestService) {
     this.requestForm = this.fb.group({
-      reciepe: this.fb.array([
+      request: this.fb.array([
         this.fb.group({
           product: ['', Validators.required],
           quantity: [0, Validators.required]
@@ -34,17 +40,22 @@ export class CreateRequestComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(res => this.products = res)
+    this.productService.getProducts().subscribe(res => {
+      this.products = res
+      this.getRequestsToBeReceived();
+    })
+
   }
+
   compare( val1: Product, val2: Product) {
     return val1.id === val2.id;
   }
   get itemControls(): any {
-    return this.requestForm.get('reciepe') as FormArray;
+    return this.requestForm.get('request') as FormArray;
   }
 
   addItem() {
-    const items = this.requestForm.get('reciepe') as FormArray;
+    const items = this.requestForm.get('request') as FormArray;
     if (!items.invalid) {
       items.push(
         this.fb.group({
@@ -56,7 +67,7 @@ export class CreateRequestComponent implements OnInit{
   }
 
   removeItem(index: any) {
-    const items = this.requestForm.get('reciepe') as FormArray;
+    const items = this.requestForm.get('request') as FormArray;
     items.removeAt(index);
   }
   findProductById(id: number): Product | null  {
@@ -67,21 +78,52 @@ export class CreateRequestComponent implements OnInit{
     }
     return null;
   }
+  isUndefined(): boolean{
+    return this.submittedRequest == undefined;
+  }
   submitForm() {
-    var reqItems = this.requestForm.value.reciepe ;
+    var reqItems = this.requestForm.value.request ;
     let prod;
     for (let i = 0; i < reqItems.length; i++) {
       prod = this.findProductById(reqItems[i].product);
       if (prod == null){
         return;
       }else{
-        this.items.push(new RequestItem(prod, reqItems[i].quantity))
+        this.items.push(new RequestItem(0,prod, reqItems[i].quantity, false))
       }
 
     }
+
     console.log(this.items)
-    console.log(this.requestForm.value.reciepe);
-    this.requestService.createRequest(this.items).subscribe();
+    console.log(this.requestForm.value.request);
+    this.requestService.createRequest(this.items).subscribe(res =>{
+      this.submitted = true;
+      this.submittedRequest = res;
+      this.getRequestsToBeReceived();
+
+
+    });
+  }
+  totalQuantity(): number{
+    let res = 0;
+    for(let i =0 ; i < this.requestForm.value.request.length; i++){
+      res = res + this.requestForm.value.request[i].quantity;
+    }
+    return res;
+  }
+
+  canSubmit(): boolean{
+    return this.requestForm.value.request.length > 0 && this.totalQuantity() > 0;
+  }
+  getRequestsToBeReceived():void{
+    this.requestService.getRequestsToBeReceived().subscribe(res => this.requestsToBeReceived = res)
+
+  }
+
+  receivedRequest(id: number):void{
+    this.requestService.receiveRequest(id).subscribe(res =>{
+      this.getRequestsToBeReceived();
+    })
   }
 
 }
