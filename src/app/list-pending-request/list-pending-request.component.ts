b6f7ec00be8b892d66f8499ceb395faebe5d328a
1201391 <1201391@isep.ipp.pt>
2024-03-31 12:47:42 +0100

    Updated list pending requests component

--- src/app/list-pending-request/list-pending-request.component.ts
+++ src/app/list-pending-request/list-pending-request.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RequestService} from "../services/request/request.service";
 import {Request} from "../models/request.model"
+import {ActivatedRoute, Router} from "@angular/router";
 @Component({
   selector: 'app-list-pending-request',
 
@@ -11,13 +12,21 @@ export class ListPendingRequestComponent implements OnInit{
   pendingRequests: Request[] = [];
 
 
-  constructor(private requestService: RequestService){
+  constructor(private activatedRoute: ActivatedRoute,private requestService: RequestService,private router: Router){
 
   }
   ngOnInit(): void {
     this.requestService.getPendingRequests().subscribe(res => {
       this.pendingRequests = res
     })
+  }
+
+
+  startCollection(id: number){
+    this.requestService.startProcessing(id).subscribe(res =>{
+      this.router.navigate(['Requests/'+ res.id])
+    })
+
   }
 
 
