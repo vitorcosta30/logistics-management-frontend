1c75405d25217e32587f2666120d5b9901b1094e
1201391 <1201391@isep.ipp.pt>
2024-04-02 18:38:41 +0100

    Small update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {CreateRequestComponent} from "./create-request/create-request.component";
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import {ListPendingRequestComponent} from "./list-pending-request/list-pending-request.component";
 import {ProcessRequestComponent} from "./process-request/process-request.component";
-import {Dashboard} from "./models/dashboard.model";
 import {DashboardComponent} from "./dashboard/dashboard.component";
-import {CanvasJS, CanvasJSAngularChartsModule} from "@canvasjs/angular-charts";
+import { CanvasJSAngularChartsModule} from "@canvasjs/angular-charts";
 
 @NgModule({
   declarations: [
@@ -28,7 +26,6 @@ import {CanvasJS, CanvasJSAngularChartsModule} from "@canvasjs/angular-charts";
     HttpClientModule,
     FormsModule, CommonModule, ReactiveFormsModule,HttpClientModule,
     CanvasJSAngularChartsModule,
-    NgMultiSelectDropDownModule.forRoot(),
 
     AppRoutingModule
   ],
